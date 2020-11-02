4ecf50d11a29f9815eed8387993e916deb62e274
mattbee <matt.bee@madetech.com>
2020-11-02 13:05:19 +0000

    Add SCSS styles and processing rules
    Based on Phil Hawksworth's excellent advice[1] this sets up inline CSS which is pre-compiled
    byt a gulp task that watches for scss file changes that then triggers and rebuild by eleventy
    when the output file causes eleventy to rebuild the site.
    
    SCSS purely used for ease as it's what I use and will get annoyed if I don't include it.
    
    Could have tried plain CSS and CSS vars and so on, but time is short so sticking with
    what I know for this iteration of the auction site.
    
    1. https://www.hawksworx.com/blog/keeping-sass-simple-and-speedy-on-eleventy/

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp    = require("gulp");
+const sass    = require("gulp-sass");
+
+/*
+  generate the css with sass
+*/
+gulp.task('css', function() {
+  return gulp.src('./scss/main.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    })
+    .on('error', sass.logError))
+    .pipe(gulp.dest('./_includes/css'));
+});
+
+/*
+  Watch folders for changess
+*/
+gulp.task("watch", function() {
+  gulp.watch('./scss/**/*.scss', gulp.parallel('css'));
+});
+
+gulp.task('default', gulp.parallel(
+  'css'
+));
