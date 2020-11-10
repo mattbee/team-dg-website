8d5cbe2132af566d7a85362ba63656eb1abc4cc0
mattbee <matt.bee@madetech.com>
2020-11-10 16:43:15 +0000

    Add Asyncronous form submission to link to correct confirmation page
    Th Netlify action was redirecting to the correct URL but showing the
    wrong content. So by submitting with JavaScript and redirecting manually
    means that the form will redirect correctly with JS enabled and work
    fine without.
    
    Next enhancment might be not leaving th page at all and using an overlay
    or replace the form with a success message, so the user still has the
    next and previous navigation available.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,58 @@
 const bidForm = document.getElementById('bidForm');
+
+if (bidForm) {
+    const serialize = function (form) {
+        var field,
+            l,
+            s = [];
+
+        if (typeof form == 'object' && form.nodeName == 'FORM') {
+            var len = form.elements.length;
+
+            for (var i = 0; i < len; i++) {
+                field = form.elements[i];
+                if (
+                    field.name &&
+                    !field.disabled &&
+                    field.type != 'button' &&
+                    field.type != 'file' &&
+                    field.type != 'hidden' &&
+                    field.type != 'reset' &&
+                    field.type != 'submit'
+                ) {
+                    if (field.type == 'select-multiple') {
+                        l = form.elements[i].options.length;
+
+                        for (var j = 0; j < l; j++) {
+                            if (field.options[j].selected) {
+                                s[s.length] = encodeURIComponent(field.name) + '=' + encodeURIComponent(field.options[j].value);
+                            }
+                        }
+                    } else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
+                        s[s.length] = encodeURIComponent(field.name) + '=' + encodeURIComponent(field.value);
+                    }
+                }
+            }
+        }
+        return s.join('&').replace(/%20/g, '+');
+    };
+    bidForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const theForm = e.currentTarget;
+        const formData = 'form-name=' + theForm.name + '&' + serialize(theForm);
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: formData
+        };
+
+        fetch(theForm.action, options)
+            .then(function (response) {
+                window.location.assign(theForm.dataset.confirmation);
+            })
+            .catch(function (error) {
+                document.getElementById('.form-error').style.display = 'block'
+            });
+    });
+}
