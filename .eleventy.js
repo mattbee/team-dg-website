af6cac76bbe498f3883c46c6a69362d1df41fccd
Matt Bee <matt@todayishould.com>
2020-11-19 23:47:42 +0000

    Override slugify filter
    The built in slug template filter leaves things like quotes and other
    characters in the url so this commit overrides the slug filter
    using the slugify package and some options to remove nasty characters
    that netlify doesn't allow in urls.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,14 @@
+const slugify = require("slugify");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addFilter("slug", (input) => {
+    const options = {
+      replacement: "-",
+      remove: /[&,+()$~%.'":*?<>{}]/g,
+      lower: true
+    };
+    return slugify(input, options);
+  });
 };
