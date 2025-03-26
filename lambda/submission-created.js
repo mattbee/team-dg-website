0b3d7a2db5358558a0ae1031038064f438709fcf
Matt Bee <matthew.bee@impacted.org.uk>
2025-03-26 16:10:26 +0000

    Update dates for 2025 auction

--- lambda/submission-created.js
+++ lambda/submission-created.js
@@ -9,8 +9,8 @@ exports.handler = function(event) {
     to: data.email, // Change to your recipient
     from: 'Friends of Goldstone School <auction@frogsauction.com>', // Change to your verified sender
     subject: `Your silent auction bid for ${data.item}`,
-    text: `Hi ${data.name}.\n\nYour bid of £${data.bid_amount} for the ${data.item} was all good.\n\nGood luck!!!\n\n\nIf you win you will be sent a payment link, which will need to be paid by the 30th May 2024, or the item will be offered to the next highest bidder.\n\n`,
-    html: `<p>Hi ${data.name},</p><p>Your bid of <strong>£${data.bid_amount}</strong> for the <strong>${data.item}</strong> was all good.</p><p>Good luck!!</p><p><br /><br />If you win you will be sent a payment link, which will need to be paid by the 30th May 2024 or the item will be offered to the next highest bidder.</p>`,
+    text: `Hi ${data.name}.\n\nYour bid of £${data.bid_amount} for the ${data.item} was all good.\n\nGood luck!!!\n\n\nIf you win you will be sent a payment link, which will need to be paid by the 18th April 2025, or the item will be offered to the next highest bidder.\n\n`,
+    html: `<p>Hi ${data.name},</p><p>Your bid of <strong>£${data.bid_amount}</strong> for the <strong>${data.item}</strong> was all good.</p><p>Good luck!!</p><p><br /><br />If you win you will be sent a payment link, which will need to be paid by the 18th April 2025 or the item will be offered to the next highest bidder.</p>`,
   }
 
   sgMail
